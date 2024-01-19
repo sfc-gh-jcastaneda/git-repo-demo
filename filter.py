80b0b71eab2f202cf9be4af5eeda9b7a5ad0489c
Juan Pablo Castañeda Rueda <143560007+Juanpacasru@users.noreply.github.com>
2024-01-19 17:42:10 -0500

    w

--- filter.py
+++ filter.py
@@ -2,5 +2,4 @@ from snowflake.snowpark.functions import col
 
 def filter_by_role(session, table_name, id):
   df = session.table(table_name)
- # return df.filter(col("role") == role)
- return df.filter(col("ID") == role)
+  return df.filter(col("ID") == role)
